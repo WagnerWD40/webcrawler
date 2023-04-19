360884a66b0d46e642f4a505138d60ce0001ae58
WagnerWD40 <wagnerwd40@gmail.com>
2023-04-19 20:23:15 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"golang.org/x/net/html"
+)
+
+var visited = map[string]bool{}
+
+func main() {
+	visitUrl("https://aprendagolang.com.br")
+}
+
+func visitUrl(url string) {
+
+	if checkIfVisited(url) {
+		return
+	}
+
+	fmt.Println(url)
+	res, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		panic(fmt.Sprintln("status diferente de 200: %d", res.StatusCode))
+	}
+
+	htmlTree, err := html.Parse(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	links := buildResult(htmlTree)
+
+	fmt.Println(len(links))
+}
+
+func checkIfVisited(url string) bool {
+	if ok := visited[url]; ok {
+		return true
+	}
+
+	visited[url] = true
+	return false
+}
+
+func buildResult(htmlTree *html.Node) []string {
+	links := []string{}
+
+	extractLinks(htmlTree, &links)
+
+	return links
+}
+
+func extractLinks(node *html.Node, links *[]string) {
+	if node.Type == html.ElementNode && node.Data == "a" {
+
+		for _, attr := range node.Attr {
+			if attr.Key != "href" {
+				continue
+			}
+
+			link, err := url.Parse(attr.Val)
+			if err != nil || link.Scheme == "" {
+				continue
+			}
+
+			url := link.String()
+
+			*links = append(*links, url)
+
+			visitUrl(url)
+		}
+	}
+
+	for child := node.FirstChild; child != nil; child = child.NextSibling {
+		extractLinks(child, links)
+	}
+}
